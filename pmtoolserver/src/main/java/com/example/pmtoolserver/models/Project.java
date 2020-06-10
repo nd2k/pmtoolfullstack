570705c378bbd578fef57b6d24370bc4b68b2423
Nicolas Decat <nicolas.decat@gmail.com>
2020-06-10 20:18:57 +0200

    Adding validation object to project

--- pmtoolserver/src/main/java/com/example/pmtoolserver/models/Project.java
+++ pmtoolserver/src/main/java/com/example/pmtoolserver/models/Project.java
@@ -7,7 +7,12 @@ import org.joda.time.DateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Document(collection = "projects")
 @Data
@@ -16,14 +21,23 @@ public class Project {
 
     @Id
     private ObjectId id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min=4, max = 5, message = "Please use 4 to 5 characters")
+    @Indexed(unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     private String startDate;
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     private String endDate;
     @CreatedDate
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     private DateTime createAt;
     @LastModifiedDate
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     private DateTime modifiedAt;
 
 }
