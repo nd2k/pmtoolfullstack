4b6e92f94c6f8151d2ef2829e111c3b4a2c1a8ce
Nicolas Decat <nicolas.decat@gmail.com>
2020-06-10 18:38:50 +0200

    initial commit - pmtoolserver

---
+++ pmtoolserver/src/main/java/com/example/pmtoolserver/PmtoolserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmtoolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtoolserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtoolserverApplication.class, args);
+	}
+
+}
---
+++ pmtoolserver/src/test/java/com/example/pmtoolserver/PmtoolserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmtoolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtoolserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
